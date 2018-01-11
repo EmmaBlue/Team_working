7e39ec391cc32f75c32390ef2ef83f04516ab7ae
Emma Blue <emmajaeblue@gmail.com>
2018-01-11 14:18:59 -0500

    Only Lightbox Without Slider

--- js/lightbox.js
+++ js/lightbox.js
@@ -6,7 +6,12 @@ jQuery(document).ready(function($) {
 		e.preventDefault();
 
 		//Get clicked link href
-		var image_href = $(this).attr("href");
+		var image_href = $(this).attr("src");
+		var text0 = document.createTextNode("This just got added");
+		var text1 = document.createTextNode("Huzzah!");
+	  console.log(e.target.id);
+		var text = ["This just got added", "Volvo", "BMW", "Huzzah!"];
+
 
 		/*
 		If the lightbox window HTML already exists in document,
@@ -19,10 +24,19 @@ jQuery(document).ready(function($) {
 		if ($('#lightbox').length > 0) { // #lightbox exists
 
 			//place href as img src value
-			$('#content').html('<img src="' + image_href + '" />');
+			$('#content').html('<img src="' + image_href + '" />' + '<p class="description">' +  text[e.currentTarget.dataset.offset] + '</p>');
+			$('#navContain').html('<div class="lightBut">' +
+				'</div>' +
+				'<div class="lightBut">' +
+				'</div>' +
+				'<div class="lightBut">' + "</div>");
+
+			//place text
+
+			//$('#content').html('<p class="description">' +  text[e.currentTarget.dataset.offset] + '</p>');
 
 			//show lightbox window - you could use .show('fast') for a transition
-			$('#lightbox').show(fast);
+			$('#lightbox').show();
 		}
 
 		else { //#lightbox does not exist - create and insert (runs 1st time only)
@@ -33,18 +47,31 @@ jQuery(document).ready(function($) {
 				'<p>Click to close</p>' +
 				'<div id="content">' + //insert clicked link's href into img src
 					'<img src="' + image_href +'" />' +
+					'<p class="description">' +  text[e.currentTarget.dataset.offset] + '</p>' +
+				'</div>' +
+				'<div id="navContain">' +
+					'<div class="lightBut">' +
+					'</div>' +
+					'<div class="lightBut">' +
+					'</div>' +
+					'<div class="lightBut">' +
+					'</div>'
 				'</div>' +
 			'</div>';
 
 			//insert lightbox HTML into page
 			$('body').append(lightbox);
+
+			$('#lightbox').on('click', function() { // live is depecrated and will throw errors
+			     $('#lightbox').hide();
+			});
 		}
 
+
+
 	});
 
-	//Click anywhere on the page to get rid of lightbox window
-	$('#lightbox').live('click', function() { //must use live, as the lightbox element is inserted into the DOM
-	     $('#lightbox').hide();
-	});
+
+
 
 });
